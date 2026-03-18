f624135c501cc47effb236ced45d4698d066c9c9
Ansh Saxena <112847912+Anshsaxenaa@users.noreply.github.com>
2026-03-18 14:55:42 +0530

    Add updateObject method to Apiclient class
    implementation of PUT Request Api

--- maven project/rest-client/src/main/java/com/api/Apiclient.java
+++ maven project/rest-client/src/main/java/com/api/Apiclient.java
@@ -42,4 +42,21 @@ public class Apiclient {
 
         System.out.println(response.body());
     }
+     public void updateObject(String id) throws Exception {
+
+        Device device = new Device("MacBook Pro Updated", 2024);
+
+        String json = mapper.writeValueAsString(device);
+
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create(URL + "/" + id))
+                .header("Content-Type", "application/json")
+                .PUT(HttpRequest.BodyPublishers.ofString(json))
+                .build();
+
+        HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+
+        System.out.println(response.body());
+    }
+}
 }
